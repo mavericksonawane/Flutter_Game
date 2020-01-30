03d3548afa56720cfde022dd6096f33ea8a78f3f
MAYUR SONAWANE <mavericksonawane@gmail.com>
2020-01-30 19:20:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# FLUTTER GAME 
+    First ever app developed by me. 
+        
+# ABOUT GAME :
+    The game is Simple. The player is positioned in the centre ( Blue ), and the enemies are running towards him ( Red ). You have to tap 3 times on each enemy to kill him. If the enemy manages to come closer to you, your health bar starts to decrease.
+
+# For More Details :
+    email: mavericksonawane@gmail.com
+    github: mavericksonawane
+    instagram: mayursonawane
+
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flame/util.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_swarm_game/game_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+  SharedPreferences storage = await SharedPreferences.getInstance();
+  GameController gameController = GameController(storage);
+  // WidgetsFlutterBinding.ensureInitialized();
+  runApp(gameController.widget);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = gameController.onTapDown;
+  flameUtil.addGestureRecognizer(tapper);
+}
+
